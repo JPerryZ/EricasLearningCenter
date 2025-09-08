4a143feb61b41f9e08c4877652d76868b4a9f014
JPerry <JLPerry121410@gmail.com>
2025-09-08 15:55:37 -0400

    ADDING SEARCH TAGS

---
+++ public/src/index.js
@@ -0,0 +1,15 @@
+import {Helmet} from 'react-helmet-async';
+
+export default function About() {
+  return (
+    <>
+      <Helmet>
+        <title>About Happy Steps Daycare - Springfield</title>
+        <meta name="description" content="Learn about our daycare philosophy, staff, and childcare programs for infants, toddlers, and preschoolers in Springfield." />
+        <meta name="keywords" content="Springfield daycare staff, childcare philosophy Springfield, safe daycare, preschool teachers" />
+      </Helmet>
+      <h1>About Us</h1>
+      <p>Our mission is to provide a safe and loving environment...</p>
+    </>
+  );
+}
